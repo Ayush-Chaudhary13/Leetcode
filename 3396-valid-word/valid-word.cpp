8c08f838a5185b3f3e51db25b7655b4ed40e8375
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-07-15 20:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (67.48%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,7 +1,32 @@
 class Solution {
 public:
     bool isValid(string word) {
-        regex pattern("(?=.*[b-df-hj-np-tv-z])(?=.*[aieou])[a-z0-9]{3,}", regex_constants::icase);
-        return regex_match(word, pattern);
+        int n=word.size();
+        if(n<3)
+        {
+            return false;
+        }
+        bool vowel=false;
+        bool consonant=false;
+        for(auto it:word)
+            {
+                if(!isalnum(it))
+                {
+                    return false;
+                }
+                if(isalpha(it))
+                {
+                    char ch=tolower(it);
+                    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                    {
+                        vowel=true;
+                    }
+                    else
+                    {
+                        consonant=true;
+                    }
+                }
+            }
+        return vowel&&consonant;
     }
 };
