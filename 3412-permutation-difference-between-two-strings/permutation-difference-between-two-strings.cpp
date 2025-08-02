7ee883fb11df52f5bc306b0e13f149a6c844ca0d
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-02 19:27:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (80.06%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.cpp
@@ -3,16 +3,11 @@ public:
     int findPermutationDifference(string s, string t) {
 
         int diff =0;
-        unordered_map<char, int> mp;
         for(int i =0; i<s.size(); i++)
         {
-            mp[s[i]] =i;
-        }
-
-        for(int i =0; i<t.size(); i++)
-        {
-            int idx = mp[t[i]];
-            diff += abs(idx - i);
+            char ch = s[i];
+            int j = t.find(ch);
+            diff += abs(j-i);
         }
         return diff;
     }
