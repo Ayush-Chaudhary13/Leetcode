5306e3fbe0c9fbebde04d14fc9d682a056b5ade9
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-02 21:51:15 +0530

    Time: 19 ms (96.55%) | Memory: 78.4 MB (97.24%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.cpp
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -1,40 +1,56 @@
-#include <vector>
-#include <map>
-#include <algorithm>
-#include <climits>
-
 class Solution {
 public:
-    long long minCost(std::vector<int>& basket1, std::vector<int>& basket2) {
-        std::map<int, int> total_counts;
-        for (int fruit : basket1) total_counts[fruit]++;
-        for (int fruit : basket2) total_counts[fruit]++;
-
-        long long min_val = LLONG_MAX;
-        for (auto const& [fruit, count] : total_counts) {
-            if (count % 2 != 0) return -1;
-            min_val = std::min(min_val, (long long)fruit);
-        }
-        
-        std::vector<long long> fruits_to_swap;
-        std::map<int, int> count1;
-        for (int fruit : basket1) count1[fruit]++;
-        
-        for (auto const& [fruit, total_count] : total_counts) {
-            int diff = count1[fruit] - (total_count / 2);
-            for (int i = 0; i < abs(diff); ++i) {
-                fruits_to_swap.push_back(fruit);
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        int n = basket1.size();
+        auto & u = basket1;
+        auto & w = basket2;
+        sort(u.begin(), u.end());
+        sort(w.begin(), w.end());
+        int a = min(u[0], w[0]);
+        a <<= 1;
+        int i, j, k, l;
+        i = j = k = l = 0;
+        while(i<n && j<n)
+        {
+            if (u[i] == w[j])
+            {
+                i++;
+                j++;
+            }
+            else if (u[i] < w[j])
+            {
+                if (i == n-1 || u[i] != u[i+1])
+                    return -1;
+                u[k++] = u[i];
+                i += 2;
+            }
+            else
+            {
+                if (j == n-1 || w[j] != w[j+1])
+                    return -1;
+                w[l++] = w[j];
+                j += 2;
             }
         }
-        
-        std::sort(fruits_to_swap.begin(), fruits_to_swap.end());
-        
-        long long total_cost = 0;
-        int swaps_to_make = fruits_to_swap.size() / 2;
-        for (int i = 0; i < swaps_to_make; ++i) {
-            total_cost += std::min(fruits_to_swap[i], 2 * min_val);
+        while (i<n)
+        {
+            if (u[i] != u[i+1])
+                return -1;
+            u[k++] = u[i];
+            i += 2;
         }
-        
-        return total_cost;
+        while (j<n)
+        {
+            if (w[j] != w[j+1])
+                return -1;
+            w[l++] = w[j];
+            j += 2;
+        }
+        long long rt = 0;
+        for (i=0; i<k; i++)
+        {
+            rt += min(min(u[i], w[k-1-i]), a);
+        }
+        return rt;
     }
 };
