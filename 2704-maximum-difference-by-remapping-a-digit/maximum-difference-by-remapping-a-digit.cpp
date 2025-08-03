2c394a82367c837d046a549d058713a0b64201e9
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-03 18:45:51 +0530

    Time: 3 ms (3.84%) | Memory: 8.1 MB (57.31%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int minMaxDifference(int num) {
+
+        string str = to_string(num);
+        string max = "";
+        string min = "";
+        char ch;
+
+        for(int i =0; i<str.size(); i++)
+        {
+            if(str[i] != '9')
+            {
+                ch = str[i];   
+                break;  
+            }
+        }
+         cout<<ch<<endl;
+        for(int i =0; i<str.size(); i++)
+         {
+            if(str[i] == ch)
+             {
+               max += '9';
+             }
+            else
+            {
+              max += str[i];
+            }
+                    
+        }
+
+        ch = str[0];
+
+        for(int i = 0; i <str.size(); i++)
+        {
+            if(str[i] == ch)
+            {
+                min += '0';
+            }
+            else
+            {
+                min += str[i];
+            }
+        }
+
+        cout<<max<<endl<<min<<endl;
+
+       return stoi(max) - stoi(min);
+        
+    }
+};
