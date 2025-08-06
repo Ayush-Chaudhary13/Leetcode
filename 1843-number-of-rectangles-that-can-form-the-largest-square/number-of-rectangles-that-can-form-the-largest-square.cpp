26e0951a3323cd689fda3ff3e49b3b127d99c604
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-06 14:56:37 +0530

    Time: 35 ms (30.18%) | Memory: 24.2 MB (19.82%) - LeetSync

--- 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.cpp
+++ 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int countGoodRectangles(vector<vector<int>>& rect) {
+         int count =0, maxi = 0;
 
-        int count =0;
-        int maxi = 0;
-
-        for(auto r : rect )
-        {
+        for(auto r : rect ) {
             int side = min(r[0], r[1]);
-
-            if(side > maxi)
-            {
+            if(side > maxi) {
                maxi = side;
                count =1;
             }
-
             else if(side == maxi)
             count++;
         }
