7dee71e1325769f9caebcfb1e302eaf38e5fcc06
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-06 17:47:08 +0530

    Time: 10 ms (8.26%) | Memory: 24.4 MB (51.76%) - LeetSync

--- 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    bool isprime(int n)
-    {
-        if(n == 1) return false;
-        if(n ==2) return true;
-        for(int i =2; i<=n/2; i++)
-        {
-            if(n%i ==0) return false;
-
+    bool isprime(int n) {
+        if (n == 1)
+            return false;
+        else if (n == 2)
+            return true;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0)
+                return false;
         }
         return true;
     }
     bool checkPrimeFrequency(vector<int>& nums) {
-        
-        unordered_map<int,int> mp;
-
-        for(auto it: nums)
-        {
+        map<int, int> mp;
+        for (auto it : nums) {
             mp[it]++;
         }
 
-        for(auto it: mp)
-        {
-            cout<<it.first<<"-"<<it.second<<endl;
-            if(isprime(it.second))return true;
+        for (auto it : mp) {
+            cout << it.first << "-" << it.second << endl;
+            if (isprime(it.second))
+                return true;
         }
         return false;
     }
