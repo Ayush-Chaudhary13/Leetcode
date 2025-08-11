b3fea29bfc7df795b9e4aba30a2736bfea7451ee
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-11 19:58:31 +0530

    Time: 3 ms (39.04%) | Memory: 8.9 MB (97.76%) - LeetSync

--- 824-number-of-lines-to-write-string/number-of-lines-to-write-string.cpp
+++ 824-number-of-lines-to-write-string/number-of-lines-to-write-string.cpp
@@ -2,27 +2,14 @@ class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
 
-        vector<int> ans;
-        int currw =0, linecnt=1;
-        
-        for(int i =0; i < s.size(); i++)
-        {
-            char ch = s[i];
-            int width = widths[ch-'a'];
-            if(currw + width > 100)
-            {
-                currw = width;
-                linecnt++;
-            }
-            else
-            {
-                currw += width;
+        vector<int> ans = {1, 0};
+        for (auto i : s) {
+            ans[1] += widths[i - 'a'];
+            if (ans[1] > 100) {
+                ans[0]++;
+                ans[1] = widths[i - 'a'];
             }
         }
-        ans.push_back(linecnt);
-        ans.push_back(currw);
-
         return ans;
-        
     }
 };
