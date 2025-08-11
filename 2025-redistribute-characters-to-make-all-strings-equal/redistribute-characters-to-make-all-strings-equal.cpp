6f18b89929f661cda5f1b9796f4863a95e0a2cbd
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-11 17:01:04 +0530

    Time: 3 ms (43.72%) | Memory: 15 MB (60.93%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -3,13 +3,11 @@ public:
     bool makeEqual(vector<string>& words) {
         int n = words.size();
         unordered_map<char,int>mp;
-
-        for(auto it: words){
-            for(auto c: it){
+         for(auto &it: words){
+            for(char c: it){
                 mp[c]++;
             }
         }
-
         for(auto it: mp){
             if(it.second % n !=0) return false;
         }
