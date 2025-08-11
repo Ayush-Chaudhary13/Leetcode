60f4b47ef47256e0f1daca6f8898c412d25848bf
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-11 19:58:52 +0530

    Time: 4 ms (24.80%) | Memory: 9.2 MB (61.92%) - LeetSync

--- 824-number-of-lines-to-write-string/number-of-lines-to-write-string.cpp
+++ 824-number-of-lines-to-write-string/number-of-lines-to-write-string.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
-
         vector<int> ans = {1, 0};
         for (auto i : s) {
             ans[1] += widths[i - 'a'];
