89f49a52a819f2dd8e41f3172f6d87410a4317e2
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-12 18:30:52 +0530

    Time: 11 ms (78.14%) | Memory: 40.5 MB (32.33%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -4,18 +4,18 @@ public:
 
         int n = nums.size();
 
-        for(int a =0; a+2*k <=n; a++) 
-        {
-            int b = a+k;
+        for (int a = 0; a + 2 * k <= n; a++) {
+            int b = a + k;
             bool valid = true;
-            for(int i =1; i<k; i++)
-            {
-                if(nums[a+i] <= nums[a+i-1] || nums[b+i] <= nums[b+i-1]) {
-                valid = false;
-                break;
+            for (int i = 1; i < k; i++) {
+                if (nums[a + i] <= nums[a + i - 1] ||
+                    nums[b + i] <= nums[b + i - 1]) {
+                    valid = false;
+                    break;
                 }
             }
-            if(valid) return true;
+            if (valid)
+                return true;
         }
 
         return false;
