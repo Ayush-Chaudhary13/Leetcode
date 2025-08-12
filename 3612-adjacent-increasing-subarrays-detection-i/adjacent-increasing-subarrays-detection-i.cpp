27cb866f3774f104952e792acb6e44fffbd1052b
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-12 18:28:57 +0530

    Time: 3 ms (98.60%) | Memory: 40.5 MB (57.21%) - LeetSync

---
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        for(int a =0; a+2*k <=n; a++) 
+        {
+            int b = a+k;
+            bool valid = true;
+            for(int i =1; i<k; i++)
+            {
+                if(nums[a+i] <= nums[a+i-1] || nums[b+i] <= nums[b+i-1]) {
+                valid = false;
+                break;
+                }
+            }
+            if(valid) return true;
+        }
+
+        return false;
+    }
+};
