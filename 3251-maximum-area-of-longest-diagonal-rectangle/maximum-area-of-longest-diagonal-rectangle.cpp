42eaf87c7a12827b985aa19f35086c179ffc3657
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-13 17:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (93.67%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -5,13 +5,11 @@ public:
         int maxd = INT_MIN, maxa = INT_MIN, count =0;
 
         for(int i=0; i<d.size(); i++) {
-            int l = d[i][0];
-            int b = d[i][1];
-            int diagonal = pow(l,2) + pow(b,2);
-            int area = l*b;
-            if(diagonal > maxd) maxa = area;
-            else if(diagonal == maxd) maxa = max(area,maxa);
-             maxd = max(maxd,diagonal);
+            int dia = pow(d[i][0],2) + pow(d[i][1],2);
+            int area = d[i][1] * d[i][0] ;
+            if(dia > maxd) maxa = area;
+            else if(dia == maxd) maxa = max(area,maxa);
+             maxd = max(maxd,dia);
         }
         return maxa;
         
