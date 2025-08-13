c35715abae46423a6fd27d73ae3ffaa0cf565c96
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-13 18:09:46 +0530

    Time: 3 ms (87.02%) | Memory: 64.9 MB (77.78%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        set<int> set1(nums1.begin(), nums1.end());
-        set<int> set2(nums2.begin(), nums2.end());
+        
+        int answer1=0,answer2=0;
 
-        int count1 = 0;
-        for (int num : nums1) {
-            if (set2.count(num)) {
-                count1++;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j]){
+                    answer1++;
+                    break;
+                }
             }
         }
 
-        int count2 = 0;
-        for (int num : nums2) {
-            if (set1.count(num)) {
-                count2++;
+        for(int i=0;i<nums2.size();i++){
+            for(int j=0;j<nums1.size();j++){
+                if(nums2[i]==nums1[j]){
+                    answer2++;
+                    break;
+                }
             }
         }
 
-        return {count1, count2};
+        return {answer1 , answer2};
+
     }
 };
