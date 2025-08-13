d8512746bb9458fca485e464183f1e9e8ce22a57
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-13 17:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (77.97%) - LeetSync

---
+++ 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumSum(vector<int>& nums) {
+
+        int sum = INT_MAX, n = nums.size();
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+               for(int k=j+1;k<n;k++){
+                    if((nums[i]<nums[j]) && (nums[k]<nums[j])){
+                         sum=min(sum, (nums[i]+nums[j]+nums[k]));
+                    }
+               } 
+           }      
+        }
+        if(sum !=INT_MAX) return sum;
+
+        return -1;
+    }
+};
