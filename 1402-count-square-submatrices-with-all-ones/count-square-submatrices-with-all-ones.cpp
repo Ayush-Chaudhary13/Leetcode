761f2f430ec67a7bd6fc1a0b0c6b7919555ecbe1
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-20 23:52:23 +0530

    Time: 8 ms (44.13%) | Memory: 27.9 MB (86.00%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,16 +1,17 @@
-class Solution {
+class Solution{
 public:
+    int dp[305][305]; 
     int countSquares(vector<vector<int>>& matrix) {
-        int res = 0;
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                if (matrix[i][j] && i && j) {
-                    matrix[i][j] = 1+ min({matrix[i - 1][j], matrix[i][j - 1],
-                                           matrix[i - 1][j - 1]}) ;
+        int ans = 0;
+        int r = matrix.size(), c = matrix[0].size();
+        for(int i = 1; i <= r; ++i){
+            for(int j = 1; j <= c; ++j){
+                if(matrix[i-1][j-1] == 1){
+                    dp[i][j] = min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]}) + 1;
+                    ans += dp[i][j];
                 }
-                res += matrix[i][j];
             }
         }
-        return res;
+        return ans;
     }
 };
