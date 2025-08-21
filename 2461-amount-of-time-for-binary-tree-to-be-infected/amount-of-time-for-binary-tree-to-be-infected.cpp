4a1330649a32649d49c85b839b36e3400e5407d2
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-21 23:40:05 +0530

    Time: 151 ms (79.28%) | Memory: 189 MB (74.82%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -6,56 +6,59 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int amountOfTime(TreeNode* root, int start) {
 
-        unordered_map<TreeNode*,TreeNode*> parentmap;
+        unordered_map<TreeNode*, TreeNode*> parentmap;
         queue<TreeNode*> q;
         q.push(root);
         TreeNode* startNode = NULL;
-        while(!q.empty()) {
-          auto node = q.front();
-          q.pop();
-          if(node->val == start) startNode = node;
-          if(node->left) {
-            parentmap[node->left] = node;
-            q.push(node->left);
-          }
-          if(node->right) {
-            parentmap[node->right] = node;
-            q.push(node->right);
-          }
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            if (node->val == start)
+                startNode = node;
+            if (node->left) {
+                parentmap[node->left] = node;
+                q.push(node->left);
+            }
+            if (node->right) {
+                parentmap[node->right] = node;
+                q.push(node->right);
+            }
         }
 
-        int time =0;
+        int time = 0;
         unordered_set<TreeNode*> visited;
         q.push(startNode);
         visited.insert(startNode);
-        while(!q.empty()) {
+        while (!q.empty()) {
             int size = q.size();
-            for(int i =0; i<size; i++) {
+            for (int i = 0; i < size; i++) {
                 auto node = q.front();
                 q.pop();
 
-                if(node->left && visited.find(node->left) == visited.end()) {
+                if (node->left && visited.find(node->left) == visited.end()) {
                     q.push(node->left);
                     visited.insert(node->left);
                 }
-                if(node->right && visited.find(node->right) == visited.end()) {
+                if (node->right && visited.find(node->right) == visited.end()) {
                     q.push(node->right);
                     visited.insert(node->right);
                 }
-if(parentmap.find(node) != parentmap.end() && visited.find(parentmap[node]) == visited.end()) {
-    q.push(parentmap[node]);
-    visited.insert(parentmap[node]);
-}
+                if (parentmap.find(node) != parentmap.end() &&
+                    visited.find(parentmap[node]) == visited.end()) {
+                    q.push(parentmap[node]);
+                    visited.insert(parentmap[node]);
+                }
             }
-            time ++;
+            time++;
         }
-        return time -1;
+        return time - 1;
     }
 };
