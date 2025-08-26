a247bce9b03bcb0b5562f199faa6fa2cb6091c2a
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-26 18:41:11 +0530

    Time: 12 ms (76.06%) | Memory: 208.1 MB (99.08%) - LeetSync

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool path(TreeNode* root, int val, string& s) {
+        if (root->val == val)
+            return true;
+        if (root->left && path(root->left, val, s))
+            s.push_back('L');
+        else if (root->right && path(root->right, val,s))
+            s.push_back('R');
+        return !s.empty();
+    }
+    string getDirections(TreeNode* root, int s, int d) {
+        string start, dest;
+        path(root, s, start);
+        path(root, d, dest);
+        int i = start.size()-1;
+        int j = dest.size()-1;
+        while (i>=0 && j>=0 ) {
+            if(start[i--] == dest[j--]) {
+            start.pop_back();
+            dest.pop_back();
+            }
+            else break;
+        }
+
+        for(auto &c: start) c = 'U';
+
+        reverse(dest.begin(),dest.end());
+        return start+dest;
+    }
+};
