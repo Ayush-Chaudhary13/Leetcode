46c6c3fc57a46b63fe576c34947a90c3d3a860e3
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-08-29 13:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (48.89%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     long long flowerGame(int n, int m) {
         long long ans =0;
-
-        long long oddx = (n+1)/2, evenx = n/2, oddy = (m+1)/2, eveny = m/2;
-        ans = (oddx*eveny) + (evenx*oddy);
+        ans = ((static_cast<long long>(n+1)/2)*(m/2)) + ((n/2)*(static_cast<long long>(m+1)/2));
         return ans;
         
     }
