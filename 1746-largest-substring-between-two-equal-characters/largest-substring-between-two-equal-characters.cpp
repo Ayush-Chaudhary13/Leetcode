5da01050b901eb340dc5504e5ff1e9f888abcb49
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-04 18:29:50 +0530

    Time: 1 ms (22.68%) | Memory: 9.1 MB (8.42%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -3,18 +3,15 @@ public:
     int maxLengthBetweenEqualCharacters(string s) {
 
         unordered_map<char, int> mpp;
-        bool found = false;
         int ans = -1;
 
         for(int i =0; i<s.size(); i++) {
             if(mpp.find(s[i]) == mpp.end()) mpp[s[i]]= i;
-            else 
-            {
+            else {
                 int x =  i - mpp[s[i]] - 1;
                 ans = max(ans,x);
             }
-            
-        }
+         }
        return ans;  
     }
 };
