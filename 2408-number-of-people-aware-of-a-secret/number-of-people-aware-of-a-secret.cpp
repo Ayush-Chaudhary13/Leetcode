27b5d56a0df2a3d0484d77ed4f3cdc6453fa9713
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-09 22:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (39.85%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        vector<long long> dp(n + 1, 0);
+        vector<long> dp(n + 1);
         dp[1] = 1;
-        long long share = 0, MOD = 1000000007;
-        for (int t = 2; t <= n; t++) {
-            if (t - delay > 0)
-                share = (share + dp[t - delay] + MOD) % MOD;
-            if (t - forget > 0)
-                share = (share - dp[t - forget] + MOD) % MOD;
-            dp[t] = share;
-        }
-        long long know = 0;
-        for (int i = n - forget + 1; i <= n; i++)
-            know = (know + dp[i]) % MOD;
-        return (int)know;
+        int share = 0, mod = 1e9 + 7, res = 0 ;
+        for (int i = 2; i <= n; ++i)
+            dp[i] = share = (share + dp[max(i - delay, 0)] - dp[max(i - forget, 0)] + mod) % mod;
+        for (int i = n - forget + 1; i <= n; ++i)
+            res = (res + dp[i]) % mod;
+        return res;
     }
 };
