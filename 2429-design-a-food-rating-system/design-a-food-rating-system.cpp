37268237d135adedd7604070541f9132d7a11289
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-17 23:09:38 +0530

    Time: 0 ms (100.00%) | Memory: 184.3 MB (5.26%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -1,26 +1,31 @@
 class FoodRatings {
-    unordered_map<string, string> foodToCuisine;
-    unordered_map<string, int> foodToRating;
-    unordered_map<string, set<pair<int, string>>> cuisineToFoods;
+    map<string, pair<string, int>> foodToCuisineAndRating;
+    map<string, map<int, set<string>>> cuisineToRatingAndFoods;
 
 public:
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        for (int i = 0; i < foods.size(); ++i) {
-            foodToCuisine[foods[i]] = cuisines[i];
-            foodToRating[foods[i]] = ratings[i];
-            cuisineToFoods[cuisines[i]].insert({-ratings[i], foods[i]});
+        for (int i = 0; i < foods.size(); i++) {
+            foodToCuisineAndRating.insert({foods[i] , {cuisines[i], ratings[i]}});
+            cuisineToRatingAndFoods[cuisines[i]][ratings[i]].insert(foods[i]);
         }
     }
-    
+
     void changeRating(string food, int newRating) {
-        string cuisine = foodToCuisine[food];
-        int oldRating = foodToRating[food];
-        cuisineToFoods[cuisine].erase({-oldRating, food});
-        cuisineToFoods[cuisine].insert({-newRating, food});
-        foodToRating[food] = newRating;
+        auto foodInfo = foodToCuisineAndRating.find(food);
+        string cuisine = foodInfo->second.first;
+        int oldRating = foodInfo->second.second;
+        foodInfo->second.second = newRating;
+
+        cuisineToRatingAndFoods[cuisine][oldRating].erase(food);
+        if (cuisineToRatingAndFoods[cuisine][oldRating].empty())
+            cuisineToRatingAndFoods[cuisine].erase(oldRating);
+
+        cuisineToRatingAndFoods[cuisine][newRating].insert(food);
     }
-    
+
     string highestRated(string cuisine) {
-        return cuisineToFoods[cuisine].begin()->second;
+        auto it = cuisineToRatingAndFoods[cuisine].rbegin();
+        return  *it->second.begin();
     }
 };
+auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
