02a09c4c4b20e513c27f25f7728c07ec27d319b8
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-24 09:37:31 +0530

    Time: 6 ms (70.19%) | Memory: 38.2 MB (15.61%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,40 +1,40 @@
-
-#define P pair<long long,int>
+#include<bits/stdc++.h>
+#define pii pair<long long,int>
 class Solution {
 public:
-   void dikjstra(int s,vector<long long>&dist,vector<vector<P>> &adj ,int n,vector<int>&count){
-    priority_queue<P,vector<P>,greater<P>>pq;
-    const int mod = 1e9+7;
-    dist[s]= 0;
-    pq.push({0,0});
-    count[0]=1;
+   void dikjstra(int s,vector<long long>&dist,vector<vector<pii>>&adj,int n,vector<int>&count){
+    priority_queue<pii,vector<pii>,greater<pii>>pq;
+    int mod = 1e9+7;
+    dist[s]=0ll;
+    pq.push({dist[s],s});
+    count[s]=1;
     while(!pq.empty()){
-        auto [d,u] = pq.top();
+        int u=pq.top().second;
+        long long D=pq.top().first;
         pq.pop();
-        if( d > dist[u]) continue;
-
-        for(auto [v,w]:adj[u]) {
-            if(dist[u] + w < dist[v]){
-                count[v] = count[u];
-                 dist[v] = dist[u] + w;
+        if(D>dist[u]) continue;
+        for(auto p:adj[u]){
+            int v=p.first;
+            int w=p.second;
+            if(dist[u]+w<dist[v]){
+                count[v]=count[u];
+                dist[v]=dist[u]+w;
                 pq.push({dist[v],v});
             }
-             else if((dist[u] + w) == dist[v]){
-                count[v] += count[u];
-                count[v] %= mod;
+             else if((dist[u]+w)==dist[v]){
+                count[v]+=count[u];
+                count[v]%=mod;
             }
 
         }
     }
 }
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<P>> adj(n);
-
-        for(auto it: roads) {
-                adj[it[0]].push_back({it[1],it[2]});
-                adj[it[1]].push_back({it[0],it[2]});
+        vector<vector<pii>>adj(n);
+        for(int i=0;i<roads.size();i++){
+                adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+                adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
         }
-
         vector<long long>dist(n, 1e18);
         vector<int>count(n,0);
         dikjstra(0,dist,adj,n,count);
