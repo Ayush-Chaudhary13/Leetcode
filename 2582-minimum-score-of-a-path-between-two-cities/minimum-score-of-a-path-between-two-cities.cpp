a238fd846301ab0353922189ca57f2ee577f0336
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-24 10:30:05 +0530

    Time: 92 ms (51.60%) | Memory: 144.3 MB (32.18%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -3,23 +3,19 @@ public:
     int minScore(int n, vector<vector<int>>& roads) {
 
         vector<vector<pair<int,int>>> adj(n+1);
-
         for(auto it: roads){
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
         vector<bool> vis(n + 1, false);
         vis[1] = 1;
-
         queue<int> q;
         q.push(1);
         int ans = INT_MAX;
 
         while(!q.empty()){
-            int node  = q.front();
-            q.pop();
-
-           for(auto [v,d]: adj[node]){
+            int node  = q.front(); q.pop();
+            for(auto [v,d]: adj[node]){
                    ans = min(ans,d);
                    if(!vis[v]){
                       vis[v] = true;
@@ -27,7 +23,6 @@ public:
                    }
            }
         }
-        return ans;
-        
+        return ans;   
     }
 };
