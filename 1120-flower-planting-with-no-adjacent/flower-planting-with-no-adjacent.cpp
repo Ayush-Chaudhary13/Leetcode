43a28cdb5e801075b3e31070d27f65336ca631a7
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-26 18:03:35 +0530

    Time: 104 ms (13.05%) | Memory: 117.7 MB (5.06%) - LeetSync

---
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void bfs(int src, vector<vector<int>>& adj, vector<int>& color){
+
+        queue<int>q;
+        q.push(src);
+        color[src-1] = 1;
+        while(!q.empty()){
+           int node = q.front();
+           q.pop();
+           int col = color[node-1];
+           for(auto it: adj[node]){
+               if(color[it-1] != 1 && color[it-1] != col) continue;
+
+               else{
+                  if(col < 4) color[it-1] = col+1;
+                  else color[it-1] = 1;
+                  q.push({it});
+               }
+           }
+        }
+    }
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+        vector<vector<int>> adj(n+1);
+        for(auto it: paths){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> color(n,1);
+
+        for(int i =1; i<n; i++){
+            bfs(i,adj,color);
+        }
+        return color;        
+    }
+};
