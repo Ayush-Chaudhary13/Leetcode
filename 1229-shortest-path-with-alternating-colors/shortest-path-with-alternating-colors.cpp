046ff24be2bb4f985027755e1f63bea894b00661
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-26 08:16:37 +0530

    Time: 8 ms (23.83%) | Memory: 18.7 MB (88.53%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -1,54 +1,36 @@
-// 1= red
-// 0 = blue
 class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red, vector<vector<int>>& blue) {
-
-        vector<vector<pair<int,int>>> adj(n);
-        for(auto it: red ){
-            adj[it[0]].push_back({it[1],1});
+        vector<vector<pair<int,int>>> list(n);
+        for(auto &r : red) {
+            int u = r[0], v = r[1];
+            list[u].push_back({v, 1});
         }
-        for(auto it: blue){
-            adj[it[0]].push_back({it[1],0});
+        for(auto &b : blue) {
+            int u = b[0], v = b[1];
+            list[u].push_back({v, 0}); 
         }
-        
-        vector<vector<bool>> vis(n, vector<bool>(2, false));
-        //vis[node][color]
-        vis[0][0] = vis[0][1] = true;
-
-        queue<vector<int>> q;
-        q.push({0,-1,0});
-        //{node,color,dist}
-
-        vector<int> dist(n,INT_MAX);
-        dist[0] = 0;
-
-        while(!q.empty()){
-            auto curr = q.front();
+        vector<vector<int>> dist(n, vector<int>(2, INT_MAX));
+        queue<tuple<int,int,int>> q;
+        dist[0][0] = dist[0][1] = 0;
+        q.push({0, 0, -1}); 
+        while(!q.empty()) {
+            auto [node, step, prev] = q.front();
             q.pop();
-            
-            int node = curr[0];
-            int color = curr[1];
-            int d = curr[2];
-            
-
-            for(auto it: adj[node]){
-                auto [neibor, ncolor] = it;
-
-                if(ncolor != color && !vis[neibor][ncolor] ){
-                    vis[neibor][ncolor] = 1;
-                    dist[neibor] = min(dist[neibor], d+1);
-                    q.push({neibor,ncolor, d+1});
+            for(auto &nbr : list[node]) {
+                int nxt = nbr.first, color = nbr.second;
+                if(color == prev) continue; 
+                if(step+1 < dist[nxt][color]) {
+                    dist[nxt][color] = step+1;
+                    q.push({nxt, step+1, color});
                 }
             }
         }
-
-        for(auto &i: dist){
-            if(i == INT_MAX){
-                i = -1;
-            }
+        vector<int> ans(n, -1);
+        for(int i = 0; i < n; i++) {
+            int d = min(dist[i][0], dist[i][1]);
+            if(d != INT_MAX) ans[i] = d;
         }
-        return dist;
-        
+        return ans;
     }
 };
