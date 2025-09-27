66e800629797fc8c4dfe714f3e5a6245d54ee665
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-27 23:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (63.36%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -5,7 +5,7 @@ public:
         double ans = 0;
         for(int i=0; i<n-2; i++) {
             for(int j=i+1; j<n-1; j++) {
-                for(int k=i+2; k<n; k++) {
+                for(int k=j+1; k<n; k++) {
                     double area = 0.5*abs(
                         1.0*points[i][0]*(points[j][1]-points[k][1]) +
                         1.0*points[j][0]*(points[k][1]-points[i][1]) +
