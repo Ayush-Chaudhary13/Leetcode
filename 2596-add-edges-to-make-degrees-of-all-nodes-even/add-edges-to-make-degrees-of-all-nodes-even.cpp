7f4bd68471806382040a83ce42e32bf15779c576
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-27 10:24:39 +0530

    Time: 227 ms (73.19%) | Memory: 194.3 MB (50.97%) - LeetSync

--- 2596-add-edges-to-make-degrees-of-all-nodes-even/add-edges-to-make-degrees-of-all-nodes-even.cpp
+++ 2596-add-edges-to-make-degrees-of-all-nodes-even/add-edges-to-make-degrees-of-all-nodes-even.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     bool isPossible(int n, vector<vector<int>>& edges) {
         vector<unordered_set<int>> adj(n + 1);
-        
         for (auto& e : edges) {
             adj[e[0]].insert(e[1]);
             adj[e[1]].insert(e[0]);
         }
-        
         vector<int> odd;
         for (int i = 1; i <= n; i++) {
-            if (adj[i].size() % 2 != 0) {
-                odd.push_back(i);
-            }
+            if (adj[i].size() % 2 != 0)odd.push_back(i);   
         }
-        
         if (odd.size() == 0) return true;
         if (odd.size() != 2 && odd.size() != 4) return false;
-        
         if (odd.size() == 2) {
             int a = odd[0], b = odd[1];
-            // Check if direct connection is possible
             if (adj[a].find(b) == adj[a].end()) return true;
-            
-            // Check if there's a common node that can connect both
             for (int i = 1; i <= n; i++) {
                 if (i != a && i != b && 
                     adj[i].find(a) == adj[i].end() && 
@@ -33,18 +24,10 @@ public:
             }
             return false;
         }
-        
-        // For 4 odd-degree nodes
         int a = odd[0], b = odd[1], c = odd[2], d = odd[3];
-        
-        // Check all possible pairings
-        if (adj[a].find(b) == adj[a].end() && adj[c].find(d) == adj[c].end()) 
-            return true;
-        if (adj[a].find(c) == adj[a].end() && adj[b].find(d) == adj[b].end()) 
-            return true;
-        if (adj[a].find(d) == adj[a].end() && adj[b].find(c) == adj[b].end()) 
-            return true;
-        
-        return false;
+    if (adj[a].find(b) == adj[a].end() && adj[c].find(d) == adj[c].end())return true;
+    if (adj[a].find(c) == adj[a].end() && adj[b].find(d) == adj[b].end())return true;
+    if (adj[a].find(d) == adj[a].end() && adj[b].find(c) == adj[b].end())return true;
+    return false;
     }
 };
