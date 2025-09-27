08238fd91e20e0508655969c1b3c7a3dd77a2339
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-09-27 09:40:31 +0530

    Time: 390 ms (14.74%) | Memory: 183.7 MB (67.15%) - LeetSync

---
+++ 2596-add-edges-to-make-degrees-of-all-nodes-even/add-edges-to-make-degrees-of-all-nodes-even.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isPossible(int n, vector<vector<int>>& edges) {
+
+        vector<int> deg(n+1);
+        map<pair<int,int>,bool> mp;
+        for(auto it: edges){
+            int u = it[0];
+            int v = it[1];
+            deg[u]++;
+            deg[v]++;
+            mp[{u,v}] = 1;
+            mp[{v,u}] = 1;
+        }
+     vector<int> odd;
+        for(int i =1; i<=n; i++){
+            if(deg[i] % 2 !=0) odd.push_back(i);      
+        }
+
+      if(odd.size() % 2 !=0 || odd.size() > 4 ) return false;
+      if(odd.size() == 0) return true;
+      if(odd.size() == 2){
+           int a = odd[0];
+           int b = odd[1];
+           if(mp[{a,b}]){
+            for(int i =1; i<=n; i++){
+                if( i == a || i == b) continue;
+                if(!mp[{i,a}] && !mp[{i,b}]) return true;
+            }
+            return false;
+           }
+           else return true;
+      }
+      else if(odd.size() == 4){
+            int a = odd[0];
+            int b = odd[1];
+            int c = odd[2];
+            int d = odd[3];
+            if(!mp[{a,b}] && !mp[{c,d}]) return true;
+            if(!mp[{a,c}] && !mp[{b,d}]) return true;
+            if(!mp[{a,d}] && !mp[{c,b}]) return true;
+            return false;
+        }
+        return false;  
+    }
+};
