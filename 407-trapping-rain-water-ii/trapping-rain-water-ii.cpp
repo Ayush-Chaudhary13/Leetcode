5bcf6047afe3fc9c484d4a93a98238972d1a3c4f
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-10-03 19:01:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.9 MB (24.13%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
@@ -1,65 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>> dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-    
-    bool check(int i, int j, int n, int m) {
-        return i >= 0 && i < n && j >= 0 && j < m;
-    }
-    
-    int trapRainWater(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        priority_queue<pair<int, pair<int, int>>, 
-                       vector<pair<int, pair<int, int>>>, 
-                       greater<pair<int, pair<int, int>>>> pq;
+    vector<int> dir = {-1,0,1,0,-1};
+    int trapRainWater(vector<vector<int>>& arr) {
         
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        
-        // Push all boundary cells into min-heap
-        for (int col = 0; col < m; col++) {
-            pq.push({mat[0][col], {0, col}});
-            pq.push({mat[n-1][col], {n-1, col}});
-            vis[0][col] = 1;
-            vis[n-1][col] = 1;
-        }
-        for (int row = 1; row < n-1; row++) {
-            pq.push({mat[row][0], {row, 0}});
-            pq.push({mat[row][m-1], {row, m-1}});
-            vis[row][0] = 1;
-            vis[row][m-1] = 1;
+        const int M = arr.size();
+        const int N = arr[0].size();
+
+        vector<vector<bool>> visited(M, vector<bool>(N, false));
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        for(int i = 0; i < N; i++) {
+            pq.push({arr[0][i], 0, i}),
+            pq.push({arr[M-1][i], M-1, i});
+            visited[0][i] = visited[M-1][i] = true;
         }
         
-        int total = 0;
-        
-        while (!pq.empty()) {
-            auto [height, cell] = pq.top();
+       
+        for(int i = 1; i < M-1; i++) {
+            pq.push({arr[i][0], i, 0}),
+            pq.push({arr[i][N-1], i, N-1});
+            visited[i][0] =  visited[i][N-1] = true;
+        }
+
+        int water =0;
+
+        while(!pq.empty()) {
+            auto it = pq.top();
             pq.pop();
-            
-            int i = cell.first;
-            int j = cell.second;
-            
-            for (auto dir : dirs) {
-                int i_ = i + dir[0];
-                int j_ = j + dir[1];
-                
-                if (check(i_, j_, n, m) && !vis[i_][j_]) {
-                    vis[i_][j_] = 1;
-                    
-                    // Water trapped at this cell
-                    if (mat[i_][j_] < height) {
-                        total += (height - mat[i_][j_]);
+            int h = it[0], r = it[1], c = it[2];
+            for(int i =0; i<4; i++){
+                int nr = r + dir[i];
+                int nc = c + dir[i+1];
+
+                if(nr >= 0 && nr < M && nc >= 0 && nc < N && !visited[nr][nc]){
+                    visited[nr][nc] = 1;
+
+                    if(arr[nr][nc] < h){
+                        water += h - arr[nr][nc];
                     }
-                    
-                    // Push next cell with updated boundary height
-                    pq.push({max(height, mat[i_][j_]), {i_, j_}});
+                    pq.push({max(h,arr[nr][nc]),nr,nc});
                 }
             }
         }
-        
-        return total;
+        return water;
     }
 };
-
-
-
 auto init = atexit([](){ ofstream("display_runtime.txt") << "0"; });
