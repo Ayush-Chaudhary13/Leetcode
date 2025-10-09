84125925f3369875b3ea1f1ed1aac96c1d1bd236
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-10-09 22:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (6.46%) - LeetSync

--- 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
@@ -17,13 +17,15 @@ public:
 
         int left = dfs(root->left);
         int right = dfs(root->right);
+
         ans += abs(left) + abs(right);
-        return root->val + left + right - 1;
+
+        return root->val + left + right -1;
+
     }
     int distributeCoins(TreeNode* root) {
-
-        ans =0;
+        ans = 0;
         dfs(root);
-       return ans;  
+        return ans;
     }
 };
