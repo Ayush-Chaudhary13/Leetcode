d87dc46857b61d8b968ff2f8701d71e99d9cc50d
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-10-23 21:13:33 +0530

    Time: 3 ms (87.54%) | Memory: 9 MB (80.81%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    bool hasSameDigits(string& s) {
-        const int n=s.size();
-        for(int i=n-1; i>1; i--){
-            for(int j=0; j<i; j++){
-                s[j]=(s[j]-'0'+s[j+1]-'0')%10+'0';
+    bool hasSameDigits(string s) {
+       while(s.size()>2){
+            int idx=0;
+            string newS=s;
+            while(idx<s.size()-1){
+                char n=(s[idx]-'0'+s[idx+1]-'0')%10;
+                newS[idx]=n+'0';
+                idx++;
             }
+            newS.pop_back();
+            s=newS;
         }
-        return s[0]==s[1];
+        return (s[0]==s[1])?true:false;
     }
 };
