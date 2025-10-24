0c6dbd790a1f6a399847a82163647e30dfcfe1f1
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-10-24 21:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (94.47%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -1,37 +1,34 @@
+#define ll long long
 class Solution {
 public:
-    int nextBeautifulNumber(int n) {
-        vector<int> list;
-        vector<int> count(10, 0);
-        generate(0, count, list);
-        sort(list.begin(), list.end());
-        for (int num : list) {
-            if (num > n) return num;
+    ll rec(ll n,ll cur,ll len,vector<ll> &ct){
+        if(len==0){
+            if(cur>n){
+                for(int i=1;i<10;i++){
+                    if(ct[i]!=i && ct[i]>0) return 0;
+                }
+                return cur;
+            }
+            return 0;
         }
-        return -1;
-    }
-
-private:
-    void generate(long num, vector<int>& count, vector<int>& list) {
-        if (num > 0 && isBeautiful(count)) {
-            list.push_back((int)num);
-        }
-        if (num > 1224444) return;
-
-        for (int d = 1; d <= 7; ++d) {
-            if (count[d] < d) {
-                count[d]++;
-                generate(num * 10 + d, count, list);
-                count[d]--;
+        ll ans=0;
+        for(int i=1;i<10 && ans==0;i++){
+            if(ct[i]<i && i-ct[i]<=len){
+                ct[i]++;
+                ans=rec(n,cur*10+i,len-1,ct);
+                ct[i]--;
             }
         }
+        return ans;
     }
-
-    bool isBeautiful(const vector<int>& count) {
-        for (int d = 1; d <= 7; ++d) {
-            if (count[d] != 0 && count[d] != d)
-                return false;
-        }
-        return true;
+    int nextBeautifulNumber(int n) {
+        string s=to_string(n);
+        ll m=s.size();
+        vector<ll> ct(10,0);
+        ll ans=rec(n,0,m,ct);
+        ct.assign(10,0);
+        ll x=rec(0,0,m+1,ct);
+        if(ans==0) ans=x;
+        return ans;
     }
 };
