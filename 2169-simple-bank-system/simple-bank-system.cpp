e12d8f838ae401a26c4c84d9adba731b590b9acf
Ayush Chaudhary <155409037+Ayush-Chaudhary13@users.noreply.github.com>
2025-10-26 21:45:52 +0530

    Time: 16 ms (36.51%) | Memory: 123.7 MB (60.18%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.cpp
+++ 2169-simple-bank-system/simple-bank-system.cpp
@@ -1,34 +1,48 @@
 class Bank {
-private:
-    vector<long long> bal;
-    int n;
-
-    bool valid(int acc) {
-        return acc > 0 && acc <= n;
-    }
+    
 
 public:
+vector<long long> bal;
+  bool isValidAc(int account){
+     return account>=1 && account<=bal.size();
+  }
     Bank(vector<long long>& balance) {
-        bal = balance;
-        n = balance.size();
+       bal=balance;
     }
-
-    bool transfer(int from, int to, long long amt) {
-        if (!valid(from) || !valid(to) || bal[from - 1] < amt) return false;
-        bal[from - 1] -= amt;
-        bal[to - 1] += amt;
-        return true;
+    
+    bool transfer(int account1, int account2, long long money) {
+        if(isValidAc(account1) && isValidAc(account2)){
+            if(bal[account1-1]>=money){
+                bal[account2-1]=bal[account2-1]+money;
+                bal[account1-1]=bal[account1-1]-money;
+                 return true;
+            }
+           
+        }
+        return false;
     }
-
-    bool deposit(int acc, long long amt) {
-        if (!valid(acc)) return false;
-        bal[acc - 1] += amt;
-        return true;
+    
+    bool deposit(int account, long long money) {
+        if(isValidAc(account)){
+            bal[account-1]+=money;
+            return true;
+        }
+        return false;
     }
-
-    bool withdraw(int acc, long long amt) {
-        if (!valid(acc) || bal[acc - 1] < amt) return false;
-        bal[acc - 1] -= amt;
-        return true;
+    
+    bool withdraw(int account, long long money) {
+          if(isValidAc(account) && bal[account-1]>=money){
+            bal[account-1]-=money;
+            return true;
+    }
+    return false;
     }
 };
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
